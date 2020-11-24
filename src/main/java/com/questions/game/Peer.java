4dfd5744d709e7256ae3996a82a8d9fe4f3972ac
hoffme <phjocoronel@gmail.com>
2020-11-23 21:09:06 -0300

    refactor

--- src/main/java/com/questions/game/Peer.java
+++ src/main/java/com/questions/game/Peer.java
@@ -6,15 +6,19 @@ import com.questions.red.Node;
 import com.questions.utils.Console;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Peer extends Node {
 
     private final PeerConfig config;
+    private final Set<String> roundRequests;
 
     public Peer(PeerConfig config) throws IOException {
         super(config.username, config.host, config.port);
 
         this.config = config;
+        roundRequests = new HashSet<>();
     }
 
     public void waitToExit() {
@@ -38,7 +42,30 @@ public class Peer extends Node {
     }
 
     private void sendNewRoundAction() {
+        Command cmd = Command
+                .newBuilder()
+                .setRoundRequest(RoundRequest
+                        .newBuilder()
+                        .setHost(Address
+                                .newBuilder()
+                                .setHost(this.host)
+                                .setPort(this.port)
+                                .setAlias(this.alias)
+                                .build()
+                        ).setTime(System.currentTimeMillis() / 1000L)
+                        .build()
+                ).build();
 
+        for (Neighbour neighbour: this.neighbours.values()) {
+            Console.println("sending round request to " + neighbour.getAlias());
+            try {
+                neighbour.send(cmd);
+            } catch (IOException e) {
+                Console.println("error to send round request: " + e.getMessage());
+            }
+        }
+
+        Console.println("round request sent");
     }
 
     private void questionnaireAction() {
@@ -70,12 +97,39 @@ public class Peer extends Node {
 
     @Override
     protected void receive(Neighbour neighbour, Command command) {
-        if (command.hasRoundRequest()) {}
+        if (command.hasRoundRequest()) this.receiveRoundRequest(neighbour, command.getRoundRequest());
         else if (command.hasQuestion()) {}
         else if (command.hasAnswer()) {}
         else if (command.hasAnswerResult()) {}
         else if (command.hasRoundResult()) {}
+        Console.println("received");
     }
 
+    private void receiveRoundRequest(Neighbour neighbourSent, RoundRequest cmd) {
+        String roundRequestId = cmd.getHost().getAlias() + cmd.getTime();
 
+        if (!this.roundRequests.contains(roundRequestId)) {
+            this.roundRequests.add(roundRequestId);
+
+            Console.println("round request receive from " + cmd.getHost().getAlias());
+
+            for (Neighbour neighbour: this.neighbours.values()) {
+                if (neighbourSent == neighbour) continue;
+
+                try {
+                    neighbour.send(Command.newBuilder().setRoundRequest(cmd).build());
+                } catch (IOException ignore) {}
+            }
+        }
+
+        if (!this.neighbours.containsKey(cmd.getHost().getAlias())) {
+            Console.print("connecting to host ... ");
+            try {
+                this.connect(cmd.getHost().getHost(), cmd.getHost().getPort());
+                Console.println("successfully");
+            } catch (IOException e) {
+                Console.println("error to connect: " + e.getMessage());
+            }
+        }
+    }
 }
